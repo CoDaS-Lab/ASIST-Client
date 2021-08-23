af26e8436b891e3073da3522ffee63d7d91df364
Valentina Blaustein <valentinablaustein@Valentinas-MacBook-Pro.local>
2021-08-23 13:54:28 -0400

    Socket issues

--- public/js/index.js
+++ public/js/index.js
@@ -44,7 +44,7 @@ var gamePlayState = new Phaser.Class({
         console.log("GamePlay");
         this.gameState = new GameState(this.mapConfig)
 
-        //socket.emit("random_victims", {'victim_locations': this.gameState.set_victims})
+        socket.emit("random_victims", {'victim_locations': this.gameState.set_victims})
 
         //this.roundDisplay = this.add.text(0,0, "Round ".concat(String(this.gameConfig.roundCount)), {color: '0x000000', fontSize: '20px'}); 
         //this.gameState.placeAtIndex(32, this.roundDisplay);
